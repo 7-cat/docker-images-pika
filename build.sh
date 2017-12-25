1fa746fcd9c4c0c6e846ca4f06e5cff2fa09e2c7
sydeevans <wj.dev@qq.com>
2017-12-25 17:28:20 +0800

    init

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git clone https://github.com/Qihoo360/pika.git /tmp/code --depth 1
+cd /tmp/code
+make
+mv /tmp/code/output /pika
+
+rm -rf /tmp/code
+
+yum -y remove snappy-devel protobuf-devel gflags-devel glog-devel gcc-c++ make git && yum clean all 
